2679e99ee1fd0802dc4a6c6df69bd53cb031ed6f
Ajinkya Wasnik <47898069+Ajinkya-W@users.noreply.github.com>
2023-10-04 01:41:30 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (29.69%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int arraySize = nums.size();
-        if(arraySize==1){
+        
+        if(nums.size()==1){
             return nums[0];
         }
     
         int a = nums[0];
         int b = max(a,nums[1]);
         
-        for (int itr = 2; itr < arraySize; itr++){
+        for (int itr = 2; itr < nums.size(); itr++){
             int c = b;
             b = max(b, nums[itr]+a);
             a = c;
